05a050735caab6f5939b225107d02d0a2578d153
syymza <syymza@gmail.com>
2015-03-06 11:34:28 +0000

    End of Task 1

--- scripts/app.js
+++ scripts/app.js
@@ -3,17 +3,30 @@
 var app = angular
   .module('TaskNinjaApp', [
     'ngAnimate',
-    'ngResource',    
-    'ngRoute',    
+    'ngResource',
+    'ngRoute',
     'firebase'
   ])
-  .constant('FURL', 'https://your-firebase.firebaseio.com/')  
+  .constant('FURL', 'https://task-ninjax.firebaseio.com/')
   .config(function ($routeProvider) {
-    $routeProvider      
+    $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html'        
+        templateUrl: 'views/main.html'
+      })
+      .when('/post', {
+        templateUrl: 'views/post.html',
+        controller: 'TaskController'
+      })
+      .when('/edit/:taskId', {
+        templateUrl: 'views/edit.html',
+        controller: 'TaskController'
+      })
+      .when('/browse', {
+        templateUrl: 'views/browse.html',
+        controller: 'TaskController'
       })
       .otherwise({
         redirectTo: '/'
       });
   });
+
