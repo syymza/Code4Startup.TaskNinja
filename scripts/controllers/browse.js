9b9062a869136f7c569e3c45c438c7ea5a1126f9
syymza <syymza@gmail.com>
2015-03-10 17:17:22 +0000

    End of Task 6

--- scripts/controllers/browse.js
+++ scripts/controllers/browse.js
@@ -27,6 +27,8 @@ app.controller('BrowseController', function ($scope,  $routeParams, toaster, Tas
 
 			$scope.isTaskCreator = Task.isCreator;
 			$scope.isOpen = Task.isOpen;
+			$scope.isAssignee =  Task.isAssignee;
+			$scope.isCompleted = Task.isCompleted;
 		}
 
 		$scope.comments = Comment.comments(task.$id);
@@ -79,6 +81,18 @@ app.controller('BrowseController', function ($scope,  $routeParams, toaster, Tas
 
 		$scope.alreadyOffered = false;
 		$scope.block = false;
+	};
+
+	$scope.acceptOffer = function (offerId, runnerId) {
+		Offer.acceptOffer($scope.selectedTask.$id, offerId, runnerId).then(function () {
+			toaster.pop('success', 'Offer is accepted.');
+		});
+	};
+
+	$scope.completeTask = function (taskId) {
+		Task.completeTask(taskId).then(function () {
+			toaster.pop('success', 'Congratulation! You have completed this task.');
+		});
 	};
 
 });
