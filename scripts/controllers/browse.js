cf9a06b34153667514c551dd8e4d2be6a8984ac2
syymza <syymza@gmail.com>
2015-03-10 12:09:12 +0000

    End of Task 4

--- scripts/controllers/browse.js
+++ scripts/controllers/browse.js
@@ -1,11 +1,13 @@
 'use strict'
 
-app.controller('BrowseController', function ($scope,  $routeParams, toaster, Task, Auth) {
+app.controller('BrowseController', function ($scope,  $routeParams, toaster, Task, Auth, Comment) {
 
 	$scope.searchTask = '';
 	$scope.tasks = Task.all;
 	$scope.signedIn = Auth.signedIn;
 	$scope.listMode = true;
+
+	$scope.user = Auth.user;
 
 	if($routeParams.taskId) {
 		var task = Task.getTask($routeParams.taskId).$asObject();
@@ -20,13 +22,26 @@ app.controller('BrowseController', function ($scope,  $routeParams, toaster, Tas
 			$scope.isTaskCreator = Task.isCreator;
 			$scope.isOpen = Task.isOpen;
 		}
+
+		$scope.comments = Comment.comments(task.$id);
 	};
 
 	$scope.cancelTask = function(taskId) {
 		Task.cancelTask(taskId).then(function () {
 			toaster.pop('success', 'This task is cancelled successfully.')
 		});
+	};
 
+	$scope.addComment = function () {
+		var comment = {
+			content: $scope.content,
+			name: $scope.user.profile.name,
+			gravatar: $scope.user.profile.gravatar
+		};
+
+		Comment.addComment($scope.selectedTask.$id, comment).then(function () {
+			$scope.content = '';
+		});
 	};
 
 });
